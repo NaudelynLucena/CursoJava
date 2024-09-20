410f8ff88113a01d6bceb204b4de2be28fe27221
NaudelynLucena <naulunau@gmail.com>
2024-09-20 12:57:47 +0200

    ejercicio1 añadido

---
+++ Ejercicio1.java
@@ -0,0 +1,20 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        String animal = "Gato";
+        String name = "Nova";
+        char sexo = 'M';
+        int edad = 4;
+        boolean vacunas = true;
+
+
+        System.out.println(animal);
+        System.out.println(name);
+        System.out.println(sexo);
+        System.out.println(edad);
+        System.out.println(vacunas);
+        System.out.println("Mi mascota es un " + animal + ". Se llama " + name + " y tiene " + edad +" meses.");
+    
+
+    }
+}

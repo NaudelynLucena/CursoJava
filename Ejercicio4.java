50ece25b792452f47f09dd7832e15967ae2e8364
NaudelynLucena <naulunau@gmail.com>
2024-09-21 18:49:27 +0200

    Intercambio usando XOR bit a bit listo.

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,4 +1,17 @@
 public class Ejercicio4 {
-    
+
+    public static void main(String[] args) {
+        
+        int manzanas = 80;
+        int peras = 50;
+
+        manzanas = manzanas + peras;
+        peras = manzanas - peras;
+        manzanas = manzanas - peras;
+
+        System.out.println(Integer.toBinaryString(manzanas));
+        System.out.println(Integer.toBinaryString(peras));
+
+    }
     
 }

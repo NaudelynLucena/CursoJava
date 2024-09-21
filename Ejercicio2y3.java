32441cefb8d66ca203836390dc397d39d283b650
NaudelynLucena <naulunau@gmail.com>
2024-09-21 17:09:32 +0200

    Ejercicio 2 y 3 añadidos en un mismo archivo

--- Ejercicio2y3.java
+++ Ejercicio2y3.java
@@ -1,6 +1,7 @@
-public class Ejercicio2 {
+public class Ejercicio2y3 {
 
     public static void main(String[] args) {
+        String Curso = "Inglés";
         int Participantes = 40;
         int Asistencias = 25;
         int Inasistencias = 15;
@@ -9,6 +10,7 @@ public class Ejercicio2 {
         Inasistencias = Asistencias - Inasistencias;
         Asistencias = Asistencias - Inasistencias;
 
+        System.out.println("Curso: " + Curso);
         System.out.println("Participantes: " + Participantes);
         System.out.println("Asistencias: " + Asistencias);
         System.out.println("Inasistencias: " + Inasistencias);

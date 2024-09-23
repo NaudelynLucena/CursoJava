d86a3ae54304e78648ea9fbfa8e2055f3053470c
NaudelynLucena <naulunau@gmail.com>
2024-09-24 00:16:52 +0200

    Area de un circulo

---
+++ Ejercicio8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio8 {
+public static void main(String[] args) { 
+    double a, r;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Introduce el radio de un círculo: ");
+    r = scanner.nextDouble();
+
+    a=Math.PI*(r*r); 
+    System. out. println("El área de un círculo de radio " +r+ " es: "+a); } 
+}

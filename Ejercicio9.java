ae4e4a173aff5fe55d96dd712c099249488366c5
NaudelynLucena <naulunau@gmail.com>
2024-09-24 00:47:15 +0200

    Interfaz GUI

---
+++ Ejercicio9.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Hello, what's your name");
+        JOptionPane.showMessageDialog(null,"Wellcome " + name); }
+}
